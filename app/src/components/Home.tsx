58377545af428adc7a15687b57c58e94e89a5c5a
Edon Abdullahu <edonabdullahu2@gmail.com>
2025-02-26 01:59:19 +0100

    Enhance Home component with loading state and download functionality; add loader and download buttons

--- app/src/components/Home.tsx
+++ app/src/components/Home.tsx
@@ -1,8 +1,8 @@
 import styles from './style.module.less';
 import logo from '../assets/AnimeHunt logo.svg';
-import { FaSearch } from 'react-icons/fa';
+import { FaDownload, FaSearch } from 'react-icons/fa';
 import { useState } from 'react';
-import axios from 'axios'; // Import axios
+import axios from 'axios';
 
 interface ApiResponse {
   data: any;
@@ -16,7 +16,10 @@ interface ErrorResponse {
 }
 
 export const Home = () => {
-  const [url, setUrl] = useState('');
+  const [url, setUrl] = useState<string>('');
+  const [loader, setLoader] = useState<boolean>(false);
+  const [readyDownload, setReadyDownload] = useState<boolean>(false);
+  const [downloadUrls, setDownloadUrls] = useState<string[]>([]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setUrl(e.target.value);
@@ -27,34 +30,66 @@ export const Home = () => {
   ): Promise<void> => {
     if (e.key === 'Enter') {
       try {
+        setLoader(true);
         const response: ApiResponse = await axios.post(
           'http://localhost:5000/extract',
           { url },
         );
 
-        console.log('Data from server:', response.data);
+        if (response.data) {
+          const urls = response.data.map(
+            (item: { request_url: string }) => item.request_url,
+          );
+          setDownloadUrls(urls);
+        }
+
+        setLoader(false);
+        setReadyDownload(true);
       } catch (error) {
         const err = error as ErrorResponse;
         console.error('Error:', err.response ? err.response.data : err.message);
+        setLoader(false);
       }
     }
   };
 
   return (
     <div className={styles.content}>
-      <div className={styles.searchBar}>
-        <img src={logo} alt="Anime Hunt" />
-        <div className={styles.inputWrapper}>
-          <FaSearch className={styles.icon} />
-          <input
-            type="text"
-            placeholder="Paste the link to download any Anime"
-            value={url}
-            onChange={handleInputChange}
-            onKeyDown={handleKeyDown}
-          />
+      {loader && (
+        <div className={styles.theLoader}>
+          <div className={styles.loader}></div>
         </div>
-      </div>
+      )}
+      {!readyDownload && (
+        <div className={styles.searchBar}>
+          <img src={logo} alt="Anime Hunt" />
+          <div className={styles.inputWrapper}>
+            <FaSearch className={styles.icon} />
+            <input
+              type="text"
+              placeholder="Paste the link to download any Anime"
+              value={url}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+            />
+          </div>
+        </div>
+      )}
+
+      {readyDownload && (
+        <div className={styles.downloadButtons}>
+          {downloadUrls.map((url, index) => (
+            <div key={index} className={styles.downloadButton}>
+              <a href={url} target="_blank" rel="noopener noreferrer">
+                <button>
+                  <FaDownload />
+                  {`Link ${index + 1}`}
+                </button>
+              </a>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
